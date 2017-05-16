103e49d7040d3ea8cabf8ae639cf0b7d91ae2608
Joshy Cyriac <j.cyriac@gmail.com>
2017-05-16 09:31:01 +0200

    new papaya version, fixed loading of npz ct and pet

--- lsreport/static/js/script.js
+++ lsreport/static/js/script.js
@@ -1,6 +1,5 @@
 $(function() {
     console.log('ready')
-
     function get(name){
         if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
             return decodeURIComponent(name[1]);
@@ -25,22 +24,13 @@ $(function() {
 
         acc_number = get('acc_number')
         ct_image = 'image_data?image_type=ct&acc_number=' + acc_number
-        console.log('Start: loading ct image')
-        console.log(ct_image)
         papayaContainers[0].viewer.loadBaseImage([ct_image], true)
-        console.log('End: loading ct image')
 
-        console.log('Start: loading pet image')
         pet_image = 'image_data?image_type=pet&acc_number=' + acc_number
-        //papayaContainers[0].viewer.loadOverlay([pet_image], true)
-        console.log('End: loading pet image')
+        papayaContainers[0].viewer.loadOverlay([pet_image], true)
 
-        console.log('start: loading label image')
         label_image = 'image_data?image_type=label&acc_number=' + acc_number
-        //papayaContainers[0].viewer.loadOverlay([label_image], true)
-        console.log('End: loading label image')
-
+        // label image not working with papaya
+        // papayaContainers[0].viewer.loadOverlay([label_image], true)
     };
-
-
 });
